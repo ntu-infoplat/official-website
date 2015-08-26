0712da473e1a6ff0ac4adca8653c2940380d0fdf
pilagod <r03725029@ntu.edu.tw>
2015-08-26 19:08:45 +0800

    home basic rwd

--- js/tuneScreen.js
+++ js/tuneScreen.js
@@ -1,13 +1,19 @@
 (function () {
-  var deviceWidth,
+  var SECTIONRATIO = 589 / 589,
+      deviceWidth,
+      topSection = $('section.top-section'),
+      sectionBackgroundImage = $('section:first-of-type > .background-wrapper > .background > img');
       contentWrapper = $('.content-wrapper'),
       navbarWidth = $('nav').width() + 50;
+
+  console.log(sectionBackgroundImage.height());
 
   window.onresize = function () {
     deviceWidth = window.innerWidth;
     console.log(deviceWidth);
     contentWrapper.width(deviceWidth * 0.915 - navbarWidth) ;
+    topSection.css('min-height', sectionBackgroundImage.height() * SECTIONRATIO);
   };
-  
+
   window.onresize();
 }());

94ccfa1937b542ab980238942e77cabc153fda78
pilagod <r03725029@ntu.edu.tw>
2015-09-04 01:57:17 +0800

    index mobile done

--- js/tuneScreen.js
+++ js/tuneScreen.js
@@ -7,14 +7,14 @@
       contentWrapper = $('.main > .content-wrapper, section > .content-wrapper'),
       navbarWidth = $('nav').width() + 50;
 
-  console.log(sectionBackgroundImage.height());
-  console.log(contentWrapper);
+  // console.log(sectionBackgroundImage.height());
+  // console.log(contentWrapper);
 
   window.onresize = function () {
     deviceWidth = window.innerWidth;
     topSectionMinHeight = sectionBackgroundImage.height() * SECTIONRATIO;
-    console.log(deviceWidth);
-    console.log(sectionBackgroundImage.height() );
+    // console.log(deviceWidth);
+    // console.log(sectionBackgroundImage.height() );
     if (deviceWidth > 1024) {
       contentWrapper.width(deviceWidth * 0.915 - navbarWidth);
     } else {
